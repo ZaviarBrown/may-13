5b4eb15471378409636ab4ec6eeab64c0c5ae08b
Zaviar Brown <brown.zaviar@gmail.com>
2024-07-18 09:15:08 -0700

    Intro to relationships <3

---
+++ 10-week/4-day/scratch.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY AUTOINCREMNT,
+  name VARCHAR(60),
+  purchase_id INTEGER
+);
+
+-- customer data
+(1, 'Ringwald', 1) -- 3
+(1, 'Waldring', 2)
+(1, 'Ringwald', 3) 
