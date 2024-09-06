0cab53577ffe141b4cf3ffad150cff598908fea1
Zaviar Brown <brown.zaviar@gmail.com>
2024-09-05 18:56:52 -0700

    Okay honestly this problem wasn't so bad, but still not worth your time

--- 17-week/4-day/3_inheritance/problems/02-triangle-with-inheritance.py
+++ 17-week/4-day/3_inheritance/problems/02-triangle-with-inheritance.py
@@ -9,16 +9,72 @@
 # semi-perimeter is the perimeter divided by 2. The square root function sqrt()
 # can be imported from the built-in math package.
 
-# Write your class here.
+# import sqrt function
+from math import sqrt
+
+
+# class we implemented in the previous problem
+class RegularPolygon:
+    type = "Polygon"
+
+    def __init__(self, num_sides, length):
+        if num_sides < 3:
+            raise Exception("A polygon must have at least 3 sides.")
+
+        self.num_sides = num_sides
+        self.length = length
+
+    def identify_polygon(self):
+        identifier_dict = {
+            3: "Triangle",
+            4: "Quadrilateral",
+            5: "Pentagon",
+            6: "Hexagon",
+            7: "Heptagon",
+            8: "Octagon",
+            9: "Nonagon",
+            10: "Decagon",
+        }
+
+        try:
+            self.type = identifier_dict[self.num_sides]
+        except KeyError:
+            self.type = f"Polygon with {self.num_sides} sides"
+
+    @classmethod
+    def polygon_factory(cls, values):
+        return [cls(num_sides, length) for num_sides, length in values]
+
+    @staticmethod
+    def get_perimeter(polygon):
+        return polygon.num_sides * polygon.length
+
+
+# triangle class
+class Triangle(RegularPolygon):
+    def __init__(self, num_sides, length):
+        if num_sides != 3:
+            raise Exception("A triangle must have exactly three sides")
+
+        super().__init__(num_sides, length)
+        self.perimeter = super().get_perimeter(self)
+        self.area = self.get_area()
+
+    def get_area(self):
+        # calculate semi-perimeter
+        s = self.perimeter / 2
+
+        # calculate area with Heron's formula
+        return sqrt(s * (s - self.length) * (s - self.length) * (s - self.length))
 
 
 triangle_a = Triangle(3, 3)
-print(triangle_a.perimeter) # 9
-print(triangle_a.area) # 3.8971...
+print(triangle_a.perimeter)  # 9
+print(triangle_a.area)  # 3.8971...
 
 triangle_b = Triangle(3, 12)
-print(triangle_b.perimeter) # 36
-print(triangle_b.area) # 62.3538...
+print(triangle_b.perimeter)  # 36
+print(triangle_b.area)  # 62.3538...
 
 triangle_c = Triangle(4, 12)
-print(triangle_c.perimeter) # Exception: A triangle must have exactly three sides
+print(triangle_c.perimeter)  # Exception: A triangle must have exactly three sides
